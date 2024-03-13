5293a3521a0d04491bb5a0fa9decfeced9b9f457
SUDHAARSAN <115058137+sudharsan3110@users.noreply.github.com>
2024-03-13 23:40:50 +0530

    Time: 62 ms (26.7%), Space: 48.9 MB (77.06%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    strs.sort();
+    var ans = ""
+    if (strs[0] == "") {
+        return ans;
+    }
+    else {
+        for (let i = 0; i < strs[0].length; i++) {
+            if(strs[0][i]!=strs[strs.length-1][i]){
+                break;
+            }
+            if (strs[0][i] === strs[strs.length - 1][i]) {
+                ans += strs[0][i];
+
+            }
+        }
+    }
+    return ans;
+};
