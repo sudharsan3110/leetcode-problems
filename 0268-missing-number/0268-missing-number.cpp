e52fbb589fb96ef424078ca692fdbc5897fb764a
SUDHAARSAN <115058137+sudharsan3110@users.noreply.github.com>
2024-07-12 08:51:34 +0530

    Time: 359 ms (5.05%), Space: 20.7 MB (26.31%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       
+        int n = nums.size();
+        for(int i =0;i<=n;i++){
+             bool flag = false;
+            for(int j = 0;j<n;j++){
+               if(i == nums[j]){
+                    flag = true;
+                    break;
+                }
+               
+            }
+             if(!flag)
+                {return i;}
+        }
+     
+       return -1;
+       }
+        
+    
+};
